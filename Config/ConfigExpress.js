84f3ad16f3011d2f3167582c083aa23299627a4d
FelipePereiraDeGodoy <godoypereirafelipe@gmail.com>
2021-05-04 13:41:03 -0300

    Adição do CORS para realização da conexão com o Front

--- Config/ConfigExpress.js
+++ Config/ConfigExpress.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const consign = require('consign');
+const cors = require('cors');
+
 
 module.exports = () => {
     const app = express();
 
+    app.use(cors());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
